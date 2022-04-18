e2af240f2255871f37ad8ec7e1e2ad5b2695580b
AlexShirin <47754331+AlexShirin@users.noreply.github.com>
2022-04-18 18:31:52 +0300

    Add files via upload
    Initial commit

---
+++ userserver/ApplicationContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace WebApp1
+{
+    public class ApplicationContext : DbContext
+    {
+        private readonly StreamWriter _logStream = new StreamWriter("log.txt", true);
+        public DbSet<Person> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=usersdb;Username=postgres;Password=123");
+            // optionsBuilder.LogTo(_logStream.WriteLine);
+            optionsBuilder.LogTo(_logStream.WriteLine, LogLevel.Debug);
+            optionsBuilder.LogTo(_logStream.WriteLine, new[] { RelationalEventId.CommandExecuted });
+            optionsBuilder.LogTo(_logStream.WriteLine, new[] { DbLoggerCategory.Database.Command.Name });
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            _logStream.Dispose();
+        }
+
+        public override async ValueTask DisposeAsync()
+        {
+            await base.DisposeAsync();
+            await _logStream.DisposeAsync();
+        }
+    }
+}
